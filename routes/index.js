e9287abbc2e639fcfac894c3e59acad78f21836f
metehankurucu <paradoxofact@gmail.com>
2020-06-03 02:24:41 +0300

    clickmeter page

--- routes/index.js
+++ routes/index.js
@@ -43,8 +43,39 @@ router.post("/url", async (req, res, next) => {
       });
     }
   } catch (error) {
-    console.log(error);
     res.render("index", {
+      title: "An error occured :(",
+      description: "Please try again later.",
+    });
+  }
+});
+
+/* GET clickmeter page. */
+router.get("/clickmeter", (req, res, next) => {
+  res.render("clickmeter", { title: "See number of clicks of your URL" });
+});
+
+/* POST get number of clicks */
+router.post("/clickmeter", async (req, res, next) => {
+  try {
+    const { url } = req.body;
+
+    const key = url.split("/").pop();
+    const item = await URL.findOne({ key });
+
+    if (item) {
+      res.render("clickmeter", {
+        title: item.opened + " times clicked",
+        description: "Your URL: " + item.url,
+      });
+    } else {
+      res.render("clickmeter", {
+        title: "Your URL is not valid",
+        description: "Please enter a valid URL",
+      });
+    }
+  } catch (error) {
+    res.render("clickmeter", {
       title: "An error occured :(",
       description: "Please try again later.",
     });
@@ -56,13 +87,20 @@ router.get("/:key", async (req, res, next) => {
   const { key } = req.params;
   try {
     const url = await URL.findOne({ key });
-    await URL.updateOne({ _id: url._id }, { opened: Number(url.opened) + 1 });
+    if (url) {
+      await URL.updateOne({ _id: url._id }, { opened: Number(url.opened) + 1 });
 
-    res.writeHead(301, { Location: url.url });
-    res.end();
+      res.writeHead(301, { Location: url.url });
+      res.end();
+    } else {
+      res.render("index", {
+        title: "There may be no such a page",
+        description: "The page you requested could not be reached",
+      });
+    }
   } catch (error) {
     res.render("index", {
-      title: "There may be no such page",
+      title: "There may be no such a page",
       description: "The page you requested could not be reached",
     });
   }
